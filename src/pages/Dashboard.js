c5733f397005c9a87cbc69382665d7cf7b79ecc3
Btshwanelo <bucibot@gmail.com>
2022-07-08 00:23:03 +0200

    added breadcrumb

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,6 +5,14 @@ const Dashboard = () => {
   return (
     <div>
       <Navbar />
+      <div className="main">
+        <div>
+          <ol  className="breadcrumb">
+            <li className="breadcrumb-item">Home  </li>
+            <li className="breadcrumb-item">Dashboard </li>
+          </ol>
+        </div>
+      </div>
     </div>
   );
 };
