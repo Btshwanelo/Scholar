3ae836c88fc31f7e275e4a080af40ea61e1ef8bb
Btshwanelo <bucibot@gmail.com>
2022-07-18 14:01:09 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import Dashboard from "./pages/Dashboard";
+import { Routes, Route, Link } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Records from "./pages/Records";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <Routes>
+      <Route path="Scholar/login" element={<Login />} />
+      <Route path="Scholar/register" element={<Register />} />
+      <Route path="Scholar/dashboard" element={<Dashboard />} />
+      <Route path="Scholar/records" element={<Records />} />
+    </Routes>
+  );
 }
 
 export default App;
