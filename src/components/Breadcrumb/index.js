c24de3b87ace4fa06864dbc05eddc75ecbab2874
Btshwanelo <bucibot@gmail.com>
2022-08-18 06:46:46 +0200

    refactored breadcrumb

--- src/components/Breadcrumb/index.js
+++ src/components/Breadcrumb/index.js
@@ -1,44 +1,31 @@
 import React from "react";
-import WithRouter from "../HOC/index";
-import style from "./style.module.css";
+
 import { useNavigate } from "react-router-dom";
+
+import "./style.css";
+import WithRouter from "../HOC/index";
 
 const Breadcrumb = (props) => {
   const pathnames = props.router.location.pathname.split("/").filter((x) => x);
+
   const navigate = useNavigate();
+
   return (
-    <div className={style.breadcrumb__container}>
-      <ol className={style.breadcrumb}>
-        {pathnames.length > 0 ? (
-          <li onClick={() => navigate("/")} className={style.breadcrumb__item}>
-            <a href="#">home</a>
-          </li>
-        ) : (
-          <li className={style.breadcrumb__item__first}>
-            home
-          </li>
-        )}
+    <div className="breadcrumb-container">
+      <ul className="breadcrumb">
         {pathnames.map((name, index) => {
           const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
           const isLast = index === pathnames.length - 1;
+
           return isLast ? (
-            <li
-              key={name}
-              className={`${style.breadcrumb__item}} ${style.breadcrumb__item__active}`}
-            >
-              {name}
-            </li>
+            <li key={name}>{name}</li>
           ) : (
-            <li
-              key={name}
-              onClick={() => navigate(routeTo)}
-              className={style.breadcrumb__item}
-            >
+            <li key={name} onClick={() => navigate(routeTo)}>
               <a href="#">{name}</a>
             </li>
           );
         })}
-      </ol>
+      </ul>
     </div>
   );
 };
