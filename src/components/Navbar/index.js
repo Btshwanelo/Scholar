65cd2e2e1b6f7ba3ca6b47604483ce8a4e9c5e03
Btshwanelo <bucibot@gmail.com>
2022-08-18 06:21:21 +0200

    refactored
    *refactored *
    -Navbar
    -Dropdown

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
-import icons from "../../assets/icons";
-import avatar from "../../assets/images/face4.jpg";
-import logo from "../../assets/images/logo.svg";
-import "./style.css";
+
 import { useNavigate } from "react-router-dom";
+
+import "./style.css";
 import Dropdown from "../Dropdown";
+import icons from "../../assets/icons";
+import logo from "../../assets/images/logo.svg";
+import avatar from "../../assets/images/face4.jpg";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -12,13 +14,13 @@ const Navbar = () => {
 
   return (
     <div className="navbar">
-      <div className="navbar__logo">
+      <div className="navbar-logo">
         <div onClick={() => navigate("/dashboard")}>
           <img src={logo} alt="logo" />
         </div>
       </div>
-      <div className="navbar__avatar" onClick={() => setDropdown(!dropdown)}>
-        <div className="navbar__avatar__wrapper">
+      <div className="navbar-avatar" onClick={() => setDropdown(!dropdown)}>
+        <div className="avatar-wrapper">
           <img src={avatar} alt="avatar" />
           <p>School One</p>
           {dropdown ? icons.avatarIconUp : icons.avatarIconDown}
