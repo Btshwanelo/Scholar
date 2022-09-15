4639ab79839af82369f171ba4d346e6ac3c29fa2
Btshwanelo <48760912+Btshwanelo@users.noreply.github.com>
2022-09-15 05:40:29 +0200

    added to in the signin page

--- src/shared/pages/SignIn/index.js
+++ src/shared/pages/SignIn/index.js
@@ -3,24 +3,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import * as Yup from 'yup';
 
+import schools from '../../assets/mock-data/schools.json';
 import { TextFieldGroup } from '../../components/';
 import { FormLayout } from '../../layouts';
 import './signin.css';
 
 const SignIn = () => {
+  const schools = schools;
   const formik = useFormik({
     initialValues: {
-      firstName: '',
-      lastName: '',
-      email: ''
+      Email: '',
+      Password: ''
     },
     validationSchema: Yup.object({
-      firstName: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
-      lastName: Yup.string().max(20, 'Must be 20 characters or less').required('Required'),
-      email: Yup.string().email('Invalid email address').required('Required')
+      Email: Yup.string().email('Invalid email address').required('Required'),
+      Password: Yup.string()
+        .required('No password provided.')
+        .min(2, 'Password is too short - should be 8 chars minimum.')
+        .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.')
     }),
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
+      //TODO: loop through schools and compare emails with the logging email, if the email matches one then compare passwords,
+      //TODO: if email doesnt match any then user is not registered, if email and password matches then route to dashboard
     }
   });
 
@@ -29,27 +34,26 @@ const SignIn = () => {
       <h4 className="form-title">Sign In</h4>
       <form className="form" onSubmit={formik.handleSubmit}>
         <TextFieldGroup
-          label={'First Name'}
-          type={'text'}
-          id={'firstName'}
-          placeholder={'First Name'}
-          error={formik.errors.firstName}
-          touched={formik.touched.firstName}
-          fieldprops={formik.getFieldProps('firstName')}
-        />
-        <TextFieldGroup
           label={'Email'}
           type={'email'}
-          id={'password'}
+          id={'Email'}
           placeholder={'Email'}
           error={formik.errors.email}
           touched={formik.touched.email}
           fieldprops={formik.getFieldProps('email')}
         />
+        <TextFieldGroup
+          label={'Password'}
+          type={'text'}
+          id={'Password'}
+          placeholder={'Password'}
+          error={formik.errors.Password}
+          touched={formik.touched.Password}
+          fieldprops={formik.getFieldProps('Password')}
+        />
         <button type="submit" className="btn btn--blue">
           Submit
         </button>
-        <button className="btn btn--grey">Cancel</button>
       </form>
       <small>
         Dont have an account ? <Link to={'/signup'}>Sign Up</Link>
